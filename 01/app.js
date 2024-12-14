27833c9d1bc50c73e6a847d4b5e4c9929e0006dc
Weronika Jankowska <wjankowska8@gmail.com>
2024-12-14 11:58:30 +0100

    task 1

--- 01/app.js
+++ 01/app.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
 const root = createRoot(document.querySelector('#root'));
 
 class App extends React.Component {
     state = {
         counter: 0,
+    };
+
+    componentDidMount() {
+        console.log('componentDidMount');
+        this.interval = setInterval(() => {
+            this.setState({
+                counter: this.state.counter + 1,
+            });
+        }, 1000);
+    }
+
+    componentDidUpdate() {
+        console.log('componentDidUpdate');
+        if (this.state.counter >= 10) {
+            root.unmount();
+        }
+    }
+    componentWillUnmount() {
+        console.log('componentWillUnmount');
+        clearInterval(this.interval);
     }
     render() {
         console.log('render');
 
-        return <h1>{ this.state.counter }</h1>
+        return <h1>{this.state.counter}</h1>;
     }
 }
 
-root.render(<App/>);
+root.render(<App />);
