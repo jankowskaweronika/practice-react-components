41391c0c21325c01387d598bd4a1883dc50eeb7b
Weronika Jankowska <wjankowska8@gmail.com>
2024-12-14 11:59:13 +0100

    task 2

--- 02/app.js
+++ 02/app.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
 const root = createRoot(document.querySelector('#root'));
 
 class Counter extends React.Component {
     state = {
         amount: 0,
-    }
-    
+    };
+    handleClick = () => {
+        this.setState({
+            amount: this.state.amount + 1,
+        });
+    };
     render() {
-        return <button>click me ({ this.state.amount })</button>
+        return (
+            <button onClick={this.handleClick}>click me ({this.state.amount})</button>
+        );
     }
 }
 
