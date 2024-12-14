0830ca088ae31b850a110706822b2daf20366467
Weronika Jankowska <wjankowska8@gmail.com>
2024-12-14 12:00:11 +0100

    task 3

--- 03/app.js
+++ 03/app.js
@@ -1,42 +1,61 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-
 const root = createRoot(document.querySelector('#root'));
 
 class Article extends React.Component {
     state = {
         comments: [],
-    }
-    
+        comment: '',
+    };
+
+    handleCommentChange = (event) => {
+        this.setState({ comment: event.target.value });
+    };
+
+    addComment = (event) => {
+        event.preventDefault();
+        const { comments, comment } = this.state;
+        if (comment) {
+            this.setState({
+                comments: [...comments, comment],
+                comment: '',
+            });
+        }
+    };
+
     render() {
-        const {title, body} = this.props;
+        const { title, body } = this.props;
         return (
             <article>
-                <h1>{ title }</h1>
-                <p>{ body }</p>
+                <h1>{title}</h1>
+                <p>{body}</p>
                 <section>
-                    <form>
+                    <form onSubmit={this.addComment}>
                         <div>
                             <label>
-                                <textarea 
-                                    style={{ "minWidth": "300px", "minHeight": "120px" }} 
-                                    name="content" 
+                                <textarea
+                                    style={{ minWidth: '300px', minHeight: '120px' }}
+                                    name="content"
+                                    value={this.state.comment}
+                                    onChange={this.handleCommentChange}
                                 />
                             </label>
                         </div>
-                        <div><input type="submit" value="dodaj komentarz" /></div>
+                        <div>
+                            <input type="submit" value="dodaj komentarz" />
+                        </div>
                     </form>
                     <ul>
-                        {/* tutaj komentarze jako <li/>, ps. tak wygląda komentarz do kodu w JSX */}
+                        {this.state.comments.map((comment, index) => (
+                            <li key={index}>{comment}</li>
+                        ))}
                     </ul>
                 </section>
             </article>
-        )
+        );
     }
 }
 
 root.render(
-    <Article 
+    <Article
         title="Programowanie jest super!"
         body="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis justo ipsum, eleifend vel quam eget, lobortis posuere arcu. In vitae eros in nisi sodales aliquam..."
     />
